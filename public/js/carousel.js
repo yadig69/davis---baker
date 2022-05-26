a450d71226e4447b1f63f5ddad3dbce0dc5315f9
yadig69 <yadig69design@gmail.com>
2022-05-25 21:00:04 -0400

    first commit

---
+++ public/js/carousel.js
@@ -0,0 +1,71 @@
+const btn = document.getElementsByClassName("btn");
+const slide = document.getElementById("slide");
+
+btn[0].onclick = function () {
+  slide.style.transform = "translateX(0px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[1].onclick = function () {
+  slide.style.transform = "translateX(0px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[2].onclick = function () {
+  slide.style.transform = "translateX(-1000px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[3].onclick = function () {
+  slide.style.transform = "translateX(-1995px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[4].onclick = function () {
+  slide.style.transform = "translateX(-3000px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[5].onclick = function () {
+  slide.style.transform = "translateX(-4000px)";
+  for (i = 0; i < 6; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+let slideIndex = 0;
+showSlides();
+
+// function showSlides() {
+//   let i;
+//   const slides = document.getElementsByClassName("slide");
+//   let btn = document.getElementsByClassName("btn");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   for (i = 0; i < btn.length; i++) {
+//     btn[i].classList.remove("active");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   btn[slideIndex - 1].classList.add("active");
+//   setTimeout(showSlides, 3000);
+// }

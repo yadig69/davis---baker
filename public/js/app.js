42cec260a20461abf8ef0bb861cf4980be158ec4
yadig69 <yadig69design@gmail.com>
2022-05-29 23:07:13 -0400

    add hide navbar on scroll

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,14 @@
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
 // });
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+};

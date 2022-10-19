b6168da9cae447c5c3d2783cd2d1cb99ecb9f761
yadig69 <yadig69design@gmail.com>
2022-10-18 20:55:03 -0400

    10/18/2022

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
 // hide navbar on scroll down
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-90px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-90px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
